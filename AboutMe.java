9aeb97174e5b40c9d55e50c93365238d549c71de
Sophia Fisher <88064054+sophia-a-fisher@users.noreply.github.com>
2021-07-27 18:47:16 -0500

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,16 @@
+
+public class AboutMe
+{
+
+	public static void main(String[] args) 
+	{
+		// print out my schedule
+		System.out.println("Sophia Fisher");
+		System.out.println("Period 1: Biology");
+		System.out.println("Period 2: Orchestra ");
+		System.out.println("Period 3: English");
+		System.out.println("Period 4: Math");
+
+	}
+
+}
---
+++ HelloWorld2.java
@@ -0,0 +1,22 @@
+/*Sophia Fisher
+ Chapter 3 - Sandbox
+ Adams - 7th period
+ September 21 2018
+ */
+
+//should print stuff on the screen
+
+
+public class HelloWorld2 
+{
+
+	public static void main(String[] args) 
+	{
+		//outputs Hello World to screen
+		System.out.println("Cats are everywhere!"); 
+
+	}
+
+}
+
+//TODO make it better
